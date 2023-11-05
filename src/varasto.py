ed36ed9178bdda69e3631a4dac33ac49d75e85f8
Iida Lassila <iida.m.lassila@helsinki.fi>
2023-11-05 17:00:11 +0200

    Adjust code to comply with pylint rules.

--- src/varasto.py
+++ src/varasto.py
@@ -2,22 +2,9 @@ class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
-            for _ in range(3):
-                if alku_saldo > 0:
-                    pass
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-
-        self.a = 0
-        self.b = 1
-        self.c = 2
-        self.d = 3
-        self.e = 4
-        self.f = 5
-        self.g = 6
-        self.h = 7
-        self.i = 8
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -29,7 +16,6 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -53,16 +39,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-
-    def method1(self): pass
-    def method2(self): pass
-    def method3(self): pass
-    def method4(self): pass
-    def method5(self): pass
-    def method6(self): pass
-    def method7(self): pass
-    def method8(self): pass
-    def method9(self): pass
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
